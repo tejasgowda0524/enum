945d7742be2036dbc2654260c40100ab743b192d
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 22:59:26 +0530

    ..

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Enum
+{
+
+    public enum days
+    {
+        sunday,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            days d = days.monday;
+            Console.WriteLine("day is " + d);
+
+            if (d==days.saturday || d==days.sunday)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("today is weeken");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("today is not weekend");
+
+
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
